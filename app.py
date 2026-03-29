db005e57a40014b6605c07ba9e8c81d4f22cb9b1
Souvik Sinha <svksnha@gmail.com>
2026-03-29 13:27:18 +0530

    Add python-version and update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ from project.models import User, BlogPost  # import models
 with app.app_context():
     db.create_all()
 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=False)
+
+# having issues with mac, so changed the port to 8000
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=False)
+    app.run(debug=True, port=8000)
